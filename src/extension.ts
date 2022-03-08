fec972ef7247a59340d02efb46e0b84d54716e7a
Ian Huff <ianhuff@CEIDCCEVIPSVC01.redmond.corp.microsoft.com>
2022-03-08 13:52:39 -0800

    remove hello world command

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,6 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('vscode-jupyter-powertoys.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from vscode-jupyter-powertoys!');
-	});
-
-	context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
